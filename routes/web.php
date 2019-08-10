9d762efa33677c98a2e848b469543ebc775a6b05
shuto.maru <answerwanashi@gmail.com>
2019-08-10 12:01:14 +0000

    modify web.php

--- routes/web.php
+++ routes/web.php
@@ -26,5 +26,5 @@ Route::post('login','Auth\LoginController@login')->name('login.post');
 Route::get('logout','Auth\LoginController@logout')->name('logout.get');
 
 Route::group(['middleware'=>['auth']],function(){
-    Route::resource('tasks','TasksController',['only'=>['index','show']]);
+    Route::resource('tasks','TasksController');
 });
