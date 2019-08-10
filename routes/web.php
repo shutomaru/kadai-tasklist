c6468f36d9bd56e16f715704576c4e7241765cbc
shuto.maru <answerwanashi@gmail.com>
2019-08-10 08:59:50 +0000

    modify web.php

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,8 @@
 |
 */
 
-Route::get('/',function(){
-    return view('TasksController@index');
-});
-
+Route::get('/','TasksController@index');
+    
 Route::resource('tasks','TasksController');
 
 Route::get('signup','Auth\RegisterController@showRegistrationForm')->name('signup.get');
