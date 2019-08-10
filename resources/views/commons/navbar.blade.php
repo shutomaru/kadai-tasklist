ab8d91142843abd3c8b735eb234f3fc6270276ee
shuto.maru <answerwanashi@gmail.com>
2019-08-10 00:44:00 +0000

    user register

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,20 @@
+<header class="mb-4">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">TaskList</a>
+                
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+                
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item">{!! link_to_route('tasks.create','新規のタスクの追加',[],['class'=>'nav-link']) !!}</li>
+                </ul>
+                <ul class="navbar navbar-nav navbar-right">
+                    <li>{!! link_to_route('signup.get','SignUp',[],['class'=>'nav-link']) !!}</li>
+                    <li><a href="#">Login</a></li>
+                </ul>
+        </div>
+    </nav>
+</header>    
