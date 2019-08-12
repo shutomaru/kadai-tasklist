c9041fbc0c10d26b2b1ad7669c039f9463134b6b
shuto.maru <answerwanashi@gmail.com>
2019-08-12 14:19:33 +0000

    modify migration

--- database/migrations/2019_08_12_231141_add_status_to_tasks_table.php
+++ database/migrations/2019_08_12_231141_add_status_to_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTasksList extends Migration
+class AddStatusToTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,8 @@ class CreateTasksList extends Migration
      */
     public function up()
     {
-        Schema::create('tasks', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('content');
-            $table->timestamps();
-            
-            
-            
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->string('status',10);
         });
     }
 
@@ -30,6 +25,8 @@ class CreateTasksList extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tasks');
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
     }
 }
